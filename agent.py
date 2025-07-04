56a754e69c10d4eb2d8213962ab0ae5e4ee1f79d
LAKAVATH RAGHURAM (2022BCS0129) <137973085+LAKAVATHRAGHURAM@users.noreply.github.com>
2025-07-04 10:59:35 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,7 +6,8 @@ from vector_store import get_best_practices
 
 import openai
 
-openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key = "your open api key"
 openai.api_base = "https://openrouter.ai/api/v1"
 
 # Then use OpenAI as normal — but it will hit OpenRouter's backend instead
